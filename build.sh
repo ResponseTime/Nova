03b8556e092e9d067894a3d0e2ce776aea69b2d2
responsetime <aayush.bhattacharjee2002@gmail.com>
2024-05-21 20:18:19 +0530

    (feat) used a makefile and used scripts to use the make

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "/mnt/d/Software Projects/CRA_GO_CLI/CLI"
-go run main.go
+make
 
--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-cd "/mnt/d/Software Projects/CRA_GO_CLI/CLI/utils"
-go test -v
-
-cd "/mnt/d/Software Projects/CRA_GO_CLI/CLI/filehandle"
-go test -v
+cd "/mnt/d/Software Projects/CRA_GO_CLI/CLI"
+make test
