70ecc07701d54db5fd7a21f4d532efc9705ce57a
KartikSindura <kartik.sindura@gmail.com>
2024-05-28 16:15:23 +0530

    default project name fix

--- CLI/main.go
+++ CLI/main.go
@@ -1,63 +1,3 @@
-// package main
-
-// import (
-// 	"flag"
-// 	"fmt"
-// 	"os"
-// 	"os/exec"
-// 	"runtime"
-
-// 	"github.com/responsetime/Nova/filehandle"
-// )
-
-// func clearScreen() {
-// 	var cmd *exec.Cmd
-// 	if runtime.GOOS == "windows" {
-// 		cmd = exec.Command("cmd", "/c", "cls")
-// 	} else {
-// 		cmd = exec.Command("clear")
-// 	}
-// 	cmd.Stdout = os.Stdout
-// 	if err := cmd.Run(); err != nil {
-// 		fmt.Println("Error clearing screen:", err)
-// 	}
-// }
-
-//	func main() {
-//		var templt string
-//		var project_name string
-//		var language string
-//		templates := []string{"React", "Vanilla", "Blank React"}
-//		flag.StringVar(&templt, "template", "vanilla", "Enter the template name")
-//		flag.StringVar(&project_name, "project-name", "Default", "Enter the project name")
-//		flag.StringVar(&language, "language", "Javascript", "Enter the language to use")
-//		flag.Parse()
-//		if len(os.Args) < 2 {
-//			fmt.Println("Enter the project name")
-//			fmt.Scan(&project_name)
-//			clearScreen()
-//			fmt.Println("Enter the template")
-//			for _, i := range templates {
-//				fmt.Println(i)
-//			}
-//			fmt.Scan(&templt)
-//			clearScreen()
-//			fmt.Println("Enter Which Language to use")
-//			fmt.Println("Javascript")
-//			fmt.Println("Typescript")
-//			fmt.Scan(&language)
-//			clearScreen()
-//		}
-//		dir, err := os.Getwd()
-//		if err != nil {
-//			fmt.Println("Error getting current directory:", err)
-//			return
-//		}
-//		filehandle.CREATE_PROJECT(dir, project_name, templt, language)
-//		fmt.Printf("%s\n", "cd "+project_name)
-//		fmt.Printf("%s\n", "npm install")
-//		fmt.Printf("%s\n", "Nova run")
-//	}
 package main
 
 import (
@@ -76,7 +16,9 @@ type menu struct {
 	templates []string
 	languages []string
 	index     int
+	screenCount int
 	err       error
+
 }
 
 func initialModel() menu {
@@ -84,13 +26,14 @@ func initialModel() menu {
 	ti.Placeholder = "Default"
 	ti.Focus()
 	ti.CharLimit = 156
-	ti.Width = 20
+	ti.Width = 30
 
 	m := menu{
 		textInput: ti,
 		templates: []string{"React", "Vanilla", "Blank React"},
 		languages: []string{"Javascript", "Typescript"},
 		index:     0,
+		screenCount: 0,
 		err:       nil,
 	}
 	return m
@@ -102,7 +45,7 @@ func (m menu) Init() tea.Cmd {
 
 func (m menu) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	last := len(m.templates) - 1
-	if screenCount == 2 {
+	if m.screenCount == 2 {
 		last = len(m.languages) - 1
 	}
 	var cmd tea.Cmd
@@ -126,13 +69,13 @@ func (m menu) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			}
 		case "enter", " ":
 			m.index = 0
-			switch screenCount {
+			switch m.screenCount {
 			case 0:
 				projectName = m.textInput.Value()
-				screenCount++
+				m.screenCount++
 			case 1:
 				template = m.templates[m.index]
-				screenCount++
+				m.screenCount++
 			case 2:
 				language = m.languages[m.index]
 				return m, tea.Quit
@@ -156,10 +99,10 @@ var headerStyle = lipgloss.NewStyle().
 
 func (m menu) View() string {
 
-	if screenCount == 0 {
-		return lipgloss.JoinVertical(lipgloss.Left, headerStyle.Render("Enter project name\n"), m.textInput.View(), helpMenuStyle.Render("\nEnter/space to confirm or Ctrl+C to exit."))
+	if m.screenCount == 0 {
+		return lipgloss.JoinVertical(lipgloss.Left, headerStyle.Render("Enter a project name\n"), m.textInput.View(), helpMenuStyle.Render("\nEnter/space to confirm or Ctrl+C to exit."))
 
-	} else if screenCount == 1 {
+	} else if m.screenCount == 1 {
 		var options []string
 		for i, o := range m.templates {
 			if i == m.index {
@@ -168,9 +111,9 @@ func (m menu) View() string {
 				options = append(options, fmt.Sprintf("   %s", o))
 			}
 		}
-		return lipgloss.JoinVertical(lipgloss.Left, headerStyle.Render("Enter the template\n"), strings.Join(options, "\n"), helpMenuStyle.Render("\nEnter/space to select, arrow keys to navigate, or Ctrl+C to exit."))
+		return lipgloss.JoinVertical(lipgloss.Left, headerStyle.Render("Select a template\n"), strings.Join(options, "\n"), helpMenuStyle.Render("\nEnter/space to select, arrow keys to navigate, or Ctrl+C to exit."))
 
-	} else if screenCount == 2 {
+	} else if m.screenCount == 2 {
 		var options []string
 		for i, o := range m.languages {
 			if i == m.index {
@@ -179,12 +122,11 @@ func (m menu) View() string {
 				options = append(options, fmt.Sprintf("   %s", o))
 			}
 		}
-		return lipgloss.JoinVertical(lipgloss.Left, headerStyle.Render("Enter the language\n"), strings.Join(options, "\n"), helpMenuStyle.Render("\nEnter/space to select, arrow keys to navigate, or Ctrl+C to exit."))
+		return lipgloss.JoinVertical(lipgloss.Left, headerStyle.Render("Select a language\n"), strings.Join(options, "\n"), helpMenuStyle.Render("\nEnter/space to select, arrow keys to navigate, or Ctrl+C to exit."))
 	}
 	return "nigasoda"
 }
 
-var screenCount int = 0
 var projectName string
 var template string
 var language string
@@ -195,12 +137,17 @@ func main() {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
-	
+
 	dir, err := os.Getwd()
 	if err != nil {
 		fmt.Println("Error getting current directory:", err)
 		return
 	}
+
+	if projectName == "" {
+		projectName = "Default"
+	}
+
 	filehandle.CREATE_PROJECT(dir, projectName, template, language)
 	fmt.Printf("%s\n", "cd "+projectName)
 	fmt.Printf("%s\n", "npm install")
